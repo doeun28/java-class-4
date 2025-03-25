7bd1660f6c37ae0c4283af18361bf00dfd9c4bf3
doeun <rlaehdms28@naver.com>
2025-03-25 11:43:28 +0900

    qww

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "김도은";
+        int age = 26;
+            System.out.println(name + "님은" + age + "살 입니다.");
+    }
+}
