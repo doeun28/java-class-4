e9c0c10be3b72fa40d2472788c974bbb1da57d67
doeun <rlaehdms28@naver.com>
2025-04-01 11:22:20 +0900

    --

---
+++ Calc/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, num3;
+
+        System.out.print("숫자를 입력하시오: ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        num3 = sc.nextInt();
+
+        System.out.println(num1 - num2 - num3);
+    }
+}
